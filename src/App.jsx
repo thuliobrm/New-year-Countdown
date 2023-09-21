629b06d8907574d2b0b5eaedc4c9399dffa0c2c9
Thulio Borba <thuliobrm@hotmail.com>
2023-09-21 19:59:00 -0300

    uploading project to github

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Title from './components/Title'
+import Counter from './components/Counter'
+import NewYear from './assets/newyear.jpg'
+import useCountdown from './hooks/useCountdown'
+import './App.css'
+
+function App() {
+  const [day, hour, minute, second] = useCountdown('Jan 1, 2024 00:00:00')
+  return (
+   <div className="app" style={{ backgroundImage: `url(${NewYear})` }}>
+    <div className="container">
+      <Title title="Contagem regressiva para 2024"/>
+      <div className="countdown-container">
+        <Counter title="Dias" number={day} />
+        <Counter title="Horas" number={hour} />
+        <Counter title="Minutos" number={minute} />
+        <Counter title="Segundos" number={second} />
+      </div>
+    </div>
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/Title.jsx
@@ -0,0 +1,8 @@
+import './Title.css'
+
+function Title({ title }) {
+  return <h1 className="title">{title}</h1>
+  
+}
+
+export default Title
